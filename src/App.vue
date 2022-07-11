e4ff369f0ceb7bae0055e062952bdd8cb7c4a494
ES-Good <evgenshtanko@ya.ru>
2022-07-12 00:47:14 +0400

    'start'

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div v-if="nodeCards.length"  class="container">
+    <NodeCards 
+    init
+    v-for="(elem, index) in nodeCards" :key="index"
+    :node="nodeCards[index]" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NodeCards from "./components/NodeCards.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { NodeCards },
+  data() {
+    return {
+      nodeCards: [],
+    };
+  },
+  created() {
+    fetch("https://functions.yandexcloud.net/d4e7argpi2fe7epuh65b")
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        this.nodeCards = data;
+
+        console.log(this.nodeCards)
+      });
   },
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f5f5f5;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000;
+  font-weight: 400;
+  height: 100vh;
+}
+
+.container {
+  width: 100%;
+  height: 100%;
+  padding: 90px;
+}
+
+.node-card {
+  background: #ffffff;
+  box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  max-width: 250px;
+  margin-bottom: 15px;
+  width: 100%;
+}
+
+.node-card__header {
+  width: 100%;
+  background: #7555f6;
+  border-radius: 5px 5px 0 0;
+}
+
+.node-card__header p {
+  color: #fff;
+  font-size: 16px;
+  font-weight: 500;
+  padding: 20px 14px;
+}
+
+.node-card__body {
+  width: 100%;
+  padding: 24px 40px;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
---
+++ src/components/NodeCards.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="node-list" :class="{'node-list_general' : init}">
+
+    <div class="node-card">
+      <div class="node-card__header">
+        <p>{{ node.title }}</p>
+      </div>
+      <div class="node-card__body">
+        <div class="node-card__in">
+          <p v-if="!init">in</p>
+        </div>
+        <div v-if="node.nodes" class="node-card__out">
+          <p v-for="(childNodes, index) in node.nodes" :key="index">out</p>
+        </div>
+      </div>
+    </div>
+
+    <NodeCards  
+    v-for="(child, index) in node.nodes" 
+    :node="child" 
+    :key="index" />
+
+  </div>
+</template>
+
+
+<script>
+
+
+export default {
+  name: 'NodeCards',
+  props: {
+    node: Object,
+    init: Boolean
+  },
+};
+
+</script>
+
+<style>
+
+.node-list_general{
+  display: flex;
+  align-items: center;
+}
+
+.node-list_general > *{
+  flex: 0 0 250px;
+  margin-right: 30px;
+}
+</style>
